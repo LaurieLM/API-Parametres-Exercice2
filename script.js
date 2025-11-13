d1e81fee65269c9d5de2a8cc66f50eb0cd150b37
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-13 15:08:08 +0100

    add search tag

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// DOM
+const bodyElement = document.querySelector("body");
+const titleSearch = document.querySelector("#search-title");
+const inputSearch = document.querySelector("#search-input");
+const btnSearch = document.querySelector("#search-btn");
+
 async function getRecipes(road) {
   const res = await fetch(`https://dummyjson.com/recipes/tag/${road}`);
   const response = await res.json();
 
   console.log(response);
 }
-getRecipes("pizza");
 
-// "Pizza",
-//     "Italian",
-//     "Vegetarian",
+btnSearch.addEventListener("click", () => {
+  let searchValue = inputSearch.value;
+  console.log(searchValue);
+  getRecipes(searchValue);
+});
