30a024ebe8ccb1fc0d00fd6ccf3f6dd2d23c1b88
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-13 15:26:21 +0100

    display recipe / tag search

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ async function getRecipes(road) {
   const response = await res.json();
 
   console.log(response);
+
+  // Display
+  bodyElement.innerHTML = "";
+  response.recipes.forEach((recipe) => {
+    const newH2 = document.createElement("h2");
+    const newUl = document.createElement("ul");
+    const newP = document.createElement("p");
+
+    newH2.textContent = recipe.name;
+    newP.textContent = recipe.instructions;
+
+    bodyElement.appendChild(newH2);
+    bodyElement.appendChild(newUl);
+    bodyElement.appendChild(newP);
+
+    recipe.ingredients.forEach((ingredient) => {
+      const newLi = document.createElement("li");
+      newLi.textContent = ingredient;
+      newUl.appendChild(newLi);
+    });
+  });
 }
 
+// Search
 btnSearch.addEventListener("click", () => {
   let searchValue = inputSearch.value;
   console.log(searchValue);
